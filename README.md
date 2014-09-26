885741174e04d1ba91ba8a2040fecb57aa87b041
Douglas Duteil <douglasduteil@gmail.com>
2014-09-26 18:53:52 +0200

    SAVE

---
+++ README.md
@@ -0,0 +1,29 @@
+# node-git-deployor [![NPM version][npm-image]][npm-url]
+
+> A git pusher helper in node. 
+
+## Install
+
+```sh
+$ npm install --save-dev node-git-deployor
+```
+
+
+## Usage
+
+```js
+
+```
+
+
+## License
+
+    Copyright © 2014 Douglas Duteil <douglasduteil@gmail.com>
+    This work is free. You can redistribute it and/or modify it under the
+    terms of the Do What The Fuck You Want To Public License, Version 2,
+    as published by Sam Hocevar. See the LICENCE file for more details.
+
+
+
+[npm-url]: https://npmjs.org/package/node-git-deployor
+[npm-image]: http://img.shields.io/npm/v/node-git-deployor.svg
---
+++ index.js
@@ -0,0 +1,143 @@
+'use strict';
+
+var path = require('path');
+var fs = require('fs');
+
+var _assign = require('lodash.assign');
+var sh = require('shelljs');
+var snakeCase = require('snake-case');
+
+module.exports = Deployor;
+
+/**
+ * @description
+ * This is the deployor system
+ *
+ * @param {Object} options - The deployment options
+ *
+ * @constructor
+ */
+function Deployor(options) {
+
+  this.options = _assign(
+    {},
+    Deployor.defaults,
+    options
+  );
+
+  // TODO move this
+  Object.keys(process.env).forEach(function (key) {
+    process.env[snakeCase(key).toUpperCase()] = process.env[key];
+  });
+
+}
+
+Deployor.defaults = {
+  cwd : process.cwd()
+};
+
+
+Deployor.verbose = true;
+
+var e = Deployor.exec = function (cmd) {
+  if (Deployor.verbose) {
+    console.log('$ ', cmd);
+  }
+  return sh.exec(cmd);
+};
+
+Deployor.cloneRepoBranch = function cloneRepoBranch(branchName, destPath, options) {
+
+  destPath = path.resolve(path.join(cwd, destPath));
+
+  _assign(process.env, {
+    branch: branchName,
+    cloneLocation: destPath
+  });
+
+
+  var res;
+
+  // Get the remote.origin.url
+  res = e('git config --get remote.origin.url');
+  if (res.code > 0) throw new Error('Can\'t get no remote.origin.url !');
+
+  process.env.repoUrl = process.env.REPO || String(res.output).split(/[\n\r]/).shift();
+  if (!process.env.repoUrl) throw new Error('No repo link !');
+
+  ///
+
+  // TODO move this
+  Object.keys(process.env).forEach(function (key) {
+    process.env[changeCase.snakeCase(key).toUpperCase()] = process.env[key];
+  });
+
+  ///
+
+  // Remove tmp file
+  e('rm -rf $CLONE_LOCATION');
+
+  ///
+
+  // Clone the repo branch to a special location (clonedRepoLocation)
+  res = e('git clone --branch=$BRANCH --single-branch $REPO_URL $CLONE_LOCATION');
+  if (res.code > 0) {
+    // try again without banch options
+    res = e('git clone $REPO_URL $CLONE_LOCATION');
+    if (res.code > 0) throw new Error('Can\'t clone !');
+  }
+
+  ///
+
+  // Go to the cloneLocation aka destPath
+  sh.cd(destPath);
+
+  if (sh.pwd() !== destPath) {
+    throw new Error('Can\'t access to the clone location : ' + destPath + ' from ' + sh.pwd());
+  }
+
+  e('git clean -f -d');
+  e('git fetch origin');
+
+  // Checkout a branch (create an orphan if it doesn't exist on the remote).
+  res = e('git ls-remote --exit-code . origin/$BRANCH');
+  if (res.code > 0) {
+    // branch doesn't exist, create an orphan
+    res = e('git checkout --orphan $BRANCH');
+    if (res.code > 0) throw new Error('Can\'t clone !');
+  } else {
+    // branch exists on remote, hard reset
+    e('git checkout $BRANCH');
+  }
+
+  return new Deployor(options);
+};
+
+Deployor.prototype = {
+  extraClean: function () {
+    // Empty the clone
+    e('git rm --ignore-unmatch -rfq \'\\.[^\\.]*\' *');
+  },
+  copy: function (srcPath) {
+    process.env.SRC_PATH = path.resolve(path.join(this.options.cwd, srcPath));
+
+    var res;
+    // Copie the targeted files
+    res = e('cp -rf $SRC_PATH/* ./');
+    if (res && res.code > 0) throw new Error(res.output);
+    res = e('cp -rf "$SRC_PATH/.[a-zA-Z0-9]*" ./');
+  },
+  commit: function (commitMessage) {
+    process.env.COMMIT_MESSAGE = commitMessage;
+    e('git add .');
+    e('git commit -m "$COMMIT_MESSAGE"');
+  },
+  tag: function (tagMessage) {
+    process.env.TAG_MESSAGE = tagMessage;
+    var res = e('git tag $TAG_MESSAGE');
+    if (res.code > 0) console.log('Can\'t tag failed, continuing !');
+  },
+  push: function () {
+    e('git push --tags origin $BRANCH');
+  }
+};
